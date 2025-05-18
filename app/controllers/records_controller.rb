7981be164ea7be663151f5d73cabe0428273d63d
Johan Rodriguez <jorodriguezal@unal.edu.co>
2025-05-18 18:10:38 +0200

    feat: :sparkles: update records index to display formatted date and conditionally show record writing button

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,7 @@
 class RecordsController < ApplicationController
+  before_action :get_records
+
   def index
-    @records = Record.all # Fetch all records from the database
   end
 
   def new # New is the action that renders the form for creating a new record
@@ -15,6 +16,15 @@ class RecordsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def get_records # This method is used to fetch records from the database
+    @records = Record.all # Fetch all records from the database
+    @has_today_record = is_today?
+  end
+
+  def is_today?
+    return @records.select { |record| record.date&.strftime("%d/%m/%Y") == DateTime.now.strftime("%d/%m/%Y") }
   end
 
   private
